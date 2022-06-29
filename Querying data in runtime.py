dddc310ce5818696484ee1f460ec8eb2b579f926
BarathkumarChandrasekaran <108383253+BarathkumarChandrasekaran@users.noreply.github.com>
2022-06-29 09:34:50 +0530

    Update Querying data in runtime.py

--- Querying data in runtime.py
+++ Querying data in runtime.py
@@ -3,7 +3,7 @@ import mysql.connector
 mydb = mysql.connector.connect(
   host="localhost",
   user="root",
-  password="Barathbk29@123",
+  password="#your password",
   database="Employee_detailssample"
 )
 
